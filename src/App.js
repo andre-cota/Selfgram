a8fb7aa73d61430e01cd6ba1265bcfc800c4ac6b
andre-cota <andre.cguimaraes@outlook.com>
2022-04-26 23:27:01 -0300

    Alterações no modo como o tailwild relaciona com o projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-function App(){
-
-    return <h1>Opa</h1>
+function App() {
+  return (
+    <h1 className="text-3x1 font-bold underline">teste</h1>
+  );
 }
 
 export default App;
